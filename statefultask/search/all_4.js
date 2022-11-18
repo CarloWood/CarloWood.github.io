686a0cb908b8ff84bca70e9b8433de5758ed0ad5
Carlo Wood <carlo@alinoe.com>
2022-11-18 20:35:34 +0100

    Fix some AIStatefulTask usage documentation.

--- statefultask/search/all_4.js
+++ statefultask/search/all_4.js
@@ -2,8 +2,7 @@ var searchData=
 [
   ['engine_0',['engine',['../unionAIStatefulTask_1_1Handler_1_1Handle.html#a4147441fa0b001ee693500e9e7697f5f',1,'AIStatefulTask::Handler::Handle']]],
   ['engine_5fh_1',['engine_h',['../structAIStatefulTask_1_1Handler.html#a49239baf5f285bd7d8161234a54c45d1a04950d91679c67187cfebbf5325ef0e4',1,'AIStatefulTask::Handler']]],
-  ['event_5fstr_2',['event_str',['../classAIStatefulTask.html#a650d995db3e1117ce9c26947aef34d90',1,'AIStatefulTask']]],
-  ['eventrequest_3',['EventRequest',['../structstatefultask_1_1ResourcePool_1_1EventRequest.html',1,'statefultask::ResourcePool']]],
-  ['example_4',['Example',['../classExample.html',1,'']]],
-  ['executing_5',['executing',['../classAIStatefulTask.html#a855cb8523b22595dc5ef69361633e7bb',1,'AIStatefulTask']]]
+  ['eventrequest_2',['EventRequest',['../structstatefultask_1_1ResourcePool_1_1EventRequest.html',1,'statefultask::ResourcePool']]],
+  ['example_3',['Example',['../classExample.html',1,'']]],
+  ['executing_4',['executing',['../classAIStatefulTask.html#a855cb8523b22595dc5ef69361633e7bb',1,'AIStatefulTask']]]
 ];
--- statefultask/search/functions_3.js
+++ statefultask/search/functions_3.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['event_5fstr_0',['event_str',['../classAIStatefulTask.html#a650d995db3e1117ce9c26947aef34d90',1,'AIStatefulTask']]],
-  ['executing_1',['executing',['../classAIStatefulTask.html#a855cb8523b22595dc5ef69361633e7bb',1,'AIStatefulTask']]]
+  ['executing_0',['executing',['../classAIStatefulTask.html#a855cb8523b22595dc5ef69361633e7bb',1,'AIStatefulTask']]]
 ];
